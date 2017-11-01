db41dfd1f6c544e5cdfc2482535bcbe8ea8751e7
Vahan <vahansargsyan1310@gmail.com>
2017-11-01 23:59:39 +0400

    4-th solved, 2-nd changed:
    1.Add solution for 4-th subtask.
    2.Add "return" from recursion in 2-nd subtask.

--- answers/No5.js
+++ answers/No5.js
@@ -2,7 +2,8 @@ const giveRandAsync = callback => {
     const f = () => callback(Math.random())
     setTimeout(f, 10)
 }
-//======> task 1
+//======> subtask 1
+// Call function 'giveRandAsync' 10 times and gather results in an array.
 const arrayOfRand = (callback, arr = []) => {
     if(arr.length === 10) {
         console.log(arr);
@@ -14,27 +15,48 @@ const arrayOfRand = (callback, arr = []) => {
 }
 // arrayOfRand(giveRandAsync);
 
-//======> task 2
-const asyncAvarge = (callback, sum = 0, i = 0) => {
-    if(i === 10) console.log(sum / 10)
-    callback(rand => {
-        asyncAvarge(callback, sum + rand, i + 1)
+//======> subtask 2
+// Call function 'giveRandAsync' 10 times, one at a time, and average the results.
+const asyncAvarge = (giveRandAsync, sum = 0, i = 0) => {
+    if(i === 10) {
+        console.log(sum / 10)
+        return;
+    }    
+    giveRandAsync(rand => {
+        asyncAvarge(giveRandAsync, sum + rand, i + 1)
     })
 }
 // asyncAvarge(giveRandAsync);
 
-//======> task 3
+//======> subtask 3
+//Call function 'giveRandAsync' 10 times, one at a time with 20 second interval, and average the results.
 const asyncAvargeWithDelay = (callback, sum = 0, i = 0) => {
     const f = (callback, sum = 0, i = 0) => {
-        console.log(`callback is in f ${callback.name}`)
         if(i === 10) {
             console.log(sum / 10);
-        } else {
-            callback(rand => {
+            return;
+        } 
+        callback(rand => {
             asyncAvargeWithDelay(callback, sum + rand, i + 1)
         })
-        }
     }
-    setTimeout(f, 190, callback, sum, i)
+    setTimeout(f, 1990, callback, sum, i)
 }
-asyncAvargeWithDelay(giveRandAsync)
+// asyncAvargeWithDelay(giveRandAsync);
+
+//======> subtask 4
+// Call function 'giveRandAsync' 10 times, all at once, and average the results.
+
+const allAtOnce = (callback, arr = []) => {
+    if(arr.length === 10) {
+        return Promise.all(arr)
+    }
+    return allAtOnce(callback, [...arr, new Promise(resolve => {
+        callback(rand => resolve(rand));
+    })])
+}
+
+allAtOnce(giveRandAsync)
+.then(res => {
+    console.log(res.reduce((a, b) => a + b) / 10);  
+});
